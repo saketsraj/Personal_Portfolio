8278a55ca1f679282bb52fd540f46d557de3402f
SAKET RAJ <puttu@SAKETs-MacBook-Air.local>
2025-03-10 00:42:20 +0530

    Scroll button added -> make responsive

--- main.js
+++ main.js
@@ -2,3 +2,33 @@ window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+//Scroll button
+const scrollTopBtn=document.querySelector(".scroll-btn");
+window.addEventListener("scroll",function(){
+    scrollTopBtn.classList.toggle("active",this.window.scrollY > 500)
+});
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+})
+
+//Navigation menu items active on page scroll (highlight on the header in which you are in)
+// window.addEventListener("scroll",() =>{
+//     const sections=document.querySelectorAll("section"); //section means <section> tag
+//     const scrollY=-window.pageYOffset;
+//     sections.forEach(current => {
+//         let sectionHeight = current.offsetHeight;
+//         let sectionTop = current.offsetTop - 50;
+//         let id = current.getAttribute("id");
+        
+//         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//           document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+//         } 
+//         else{
+//           document.querySelector(".nav-items a[href*=" + id + " ]").classList.remove("active");
+//         }
+//     });
+// });
+
+
