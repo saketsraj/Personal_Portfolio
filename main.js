1f0221c66faea78ebb20e9e9087f500ff9bf36e1
SAKET RAJ <puttu@SAKETs-MacBook-Air.local>
2025-03-10 05:10:59 +0530

    Responsivegit add .

--- main.js
+++ main.js
@@ -31,4 +31,22 @@ scrollTopBtn.addEventListener("click", () => {
 //     });
 // });
 
+//Responsive navigation menu toggle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItem = document.querySelectorAll(".nav-items a");
 
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+
+navItem.forEach((navItem) => {
+    navItem.addEventListener("click",() => {
+        navigation.classList.remove("active");
+    })
+})
